7b065049159f97ab1db970e28607b0e7d1b36554
James Preston <jamescpreston93@gmail.com>
2019-04-06 18:44:59 +0100

    Spawn enemies in the level

---
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform enemiesContainer;
+
+    private Transform[] spawnPoints;
+
+    private float spawnRate = 5.0f;
+    private float nextSpawnTime;
+
+    private Random randomNumberGenerator;
+
+    void Awake()
+    {
+        nextSpawnTime = Time.time + spawnRate;
+        randomNumberGenerator = new Random();
+        spawnPoints = transform
+            .Cast<Transform>()
+            .ToArray();
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + spawnRate;
+
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        // Pick a random spawn point
+        var index = randomNumberGenerator.Next(spawnPoints.Length);
+        var spawnPoint = spawnPoints[index].transform;
+
+        // Spawn an enemy and group in enemy container
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation, enemiesContainer);
+    }
+}
