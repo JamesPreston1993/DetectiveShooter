aedc863fd9a3ba8daf9eee9278e42fd45b3d709a
James Preston <jamescpreston93@gmail.com>
2019-04-06 23:37:34 +0100

    Draw lines between movement nodes on the map

--- Assets/Scripts/Level/MovementNode.cs
+++ Assets/Scripts/Level/MovementNode.cs
@@ -12,6 +12,22 @@ public class MovementNode : MonoBehaviour
         randomNumberGenerator = new Random();
     }
 
+    void Start()
+    {
+        var lineRenderer = gameObject.AddComponent<LineRenderer>();
+        Debug.Log(lineRenderer);
+        foreach (var node in nextNodes)
+        {
+            var direction = node.transform.position - transform.position;
+            lineRenderer.SetPositions(new Vector3[] {
+                transform.position,
+                node.transform.position
+            });
+            lineRenderer.startColor = lineRenderer.endColor = Color.magenta;
+            lineRenderer.startWidth =  lineRenderer.endWidth = 0.1f;
+        }
+    }
+
     public GameObject PickRandomNextNode()
     {
         var index = randomNumberGenerator.Next(nextNodes.Length);
