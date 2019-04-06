49f5d0cc559da6999a14b7b06eb2822364e52446
James Preston <jamescpreston93@gmail.com>
2019-04-06 16:38:24 +0100

    Set limited fire rate

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private float fireRate = 0.5f;
+    private float nextFireTime;
+
+    private int shotsFired;
+
+	void Update ()
+    {
+        // Check if player is shooting
+        var isPlayerShooting = Input.GetButton("Fire1");
+
+        // Check if player can shoot
+        var canShoot = Time.time > nextFireTime;
+
+        if (isPlayerShooting && canShoot)
+        {
+            nextFireTime = Time.time + fireRate;
+            Debug.Log("Bang! Shots fired: " + ++shotsFired);
+        }
+    }
+}
