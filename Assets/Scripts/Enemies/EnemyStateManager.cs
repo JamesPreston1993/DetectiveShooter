70bd67da38e1c9e0416802410c071d6d0c9f00f7
James Preston <jamescpreston93@gmail.com>
2019-04-07 16:43:29 +0100

    Add state manager to shoot player when nearby and move if not

---
+++ Assets/Scripts/Enemies/EnemyStateManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStateManager : MonoBehaviour
+{
+    private AttackTargets attackTargets;
+    private EnemyMovement enemyMovement;
+
+    void Start()
+    {
+        attackTargets = GetComponent<AttackTargets>();
+        enemyMovement = GetComponent<EnemyMovement>();
+
+        SetMoveState();
+    }
+
+    private void SetMoveState()
+    {
+        attackTargets.enabled = false;
+        enemyMovement.enabled = true;
+    }
+
+    private void SetAttackState()
+    {
+        attackTargets.enabled = true;
+        enemyMovement.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SetAttackState();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SetMoveState();
+        }
+    }
+}
