0ac01faba9d203a0b2f1fb64f57c359b64819727
James Preston <jamescpreston93@gmail.com>
2019-04-20 19:28:49 +0100

    Destroy money if not picked up in 3 seconds

---
+++ Assets/Scripts/Level/MoneyTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoneyTimer : MonoBehaviour
+{
+    private float startTime;
+    public float destroyAfterTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time > startTime + destroyAfterTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
