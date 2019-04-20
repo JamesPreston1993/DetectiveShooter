50766218327af58053a71cb186c7fc0fc8cd7282
James Preston <jamescpreston93@gmail.com>
2019-04-20 17:56:10 +0100

    Play sound when enemy dies and display blood and move enemy back when shot

--- Assets/Scripts/Enemies/TakeDamage.cs
+++ Assets/Scripts/Enemies/TakeDamage.cs
@@ -4,13 +4,39 @@ public class TakeDamage : MonoBehaviour
 {
     private int maxHealth = 3;
 
+    public GameObject bloodEffect;
+    public AudioClip deathSound;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void Damage(int damage)
     {
-        Debug.Log(name + " was damaged!");
+        DamageEffect();
+
         maxHealth -= damage;
         if (maxHealth <= 0)
         {
-            Destroy(gameObject);
+            DeathEffect();
+            Destroy(gameObject, 0.4f);
         }
+    }
+
+    private void DamageEffect()
+    {
+        Instantiate(bloodEffect, transform);
+        transform.position -= transform.forward * 0.8f;
+    }
+
+    private void DeathEffect()
+    {
+        audioSource.Stop();
+        audioSource.clip = deathSound;
+        audioSource.Play();
+        Instantiate(bloodEffect, transform);
     }
 }
