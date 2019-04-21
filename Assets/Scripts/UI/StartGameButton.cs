c641ce60cde49ac61a7050d0e4569a1ede76a10e
James Preston <jamescpreston93@gmail.com>
2019-04-21 17:58:39 +0100

    Add interactivity to button

---
+++ Assets/Scripts/UI/StartGameButton.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartGameButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private TextMeshProUGUI buttonText;
+
+    void Awake()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.SetText("Ready?");
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Clicked");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.SetText("Ready!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.SetText("Ready?");
+    }
+}
