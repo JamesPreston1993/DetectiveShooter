97438fd7c6ef47a083509eb2e5ed227b7231d7c0
James Preston <jamescpreston93@gmail.com>
2019-04-21 17:13:27 +0100

    Spawn ammo around the map

---
+++ Assets/Scripts/Level/AmmoPickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    public bool isAmmoAvailable;
+    public GameObject ammoBox;
+
+    public void Awake()
+    {
+        ammoBox.SetActive(false);
+    }
+
+    public void Activate()
+    {
+        isAmmoAvailable = true;
+        ammoBox.SetActive(true);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerShoot>().AddAmmo(12);
+            isAmmoAvailable = false;
+            ammoBox.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Level/AmmoSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class AmmoSpawner : MonoBehaviour
+{
+    private AmmoPickup[] pickups;
+
+    private float spawnRate = 20.0f;
+    private float initalSpawnDelay = 40.0f;
+    private float nextSpawnTime;
+
+    private Random randomNumberGenerator;
+
+    void Awake()
+    {
+        nextSpawnTime = Time.time + initalSpawnDelay;
+        randomNumberGenerator = new Random();
+        pickups = GetComponentsInChildren<AmmoPickup>();
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + spawnRate;
+            var availablePickups = pickups
+                .ToList()
+                .Where(p => !p.isAmmoAvailable);
+
+            if (availablePickups.Any())
+            {
+                var index = randomNumberGenerator.Next(availablePickups.Count());
+                availablePickups.ElementAt(index).Activate();
+            }
+        }
+    }
+}
